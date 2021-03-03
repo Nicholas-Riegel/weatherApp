7cff31387a342c954dc5acc9cb403d52f11f1b90
nickr <nicholaspriegel@gmail.com>
2021-03-03 23:08:24 +0100

    minor fixes

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ const city = document.querySelector('.city')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const img = document.querySelector('img')
-
 let searchValue
 
 input.onkeypress = searchWeather
 
+function searchWeather(e) {
+
+  if (e.key === 'Enter') {
+    
+    weather.textContent = null
+    city.textContent = null
+    temperature.textContent = null
+    img.src = null
+    
+    if (input.value) {
+      searchValue = input.value
+      input.value = null
+      getWeather()
+
+    }
+  }
+}
+
 async function getWeather() {
   
   let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=871788a612fd12ef307d9526a30dc916`, { mode: 'cors' })
-  console.log(result);
+
   if (result.ok) {
+
     let result2 = await result.json()
-    console.log(result2);
 
     city.textContent = `${result2.name}, ${result2.sys.country}`
     
@@ -27,22 +44,8 @@ async function getWeather() {
     temperature.textContent = `${celsius}° Celsius / ${farenheit}° Farenheit`
   
   } else {
-    alert('Location not found. Please try again.');
-  }
-}
-// getWeather()
-function searchWeather(e) {
-  if (e.key === 'Enter') {
-    weather.textContent = null
-    city.textContent = null
-    temperature.textContent = null
-    img.src=null
-    if (input.value) {
-      searchValue = input.value
-      input.value = null
-      getWeather()
-    }
-  }
-}
 
-//fetch is always successful, even when it's not, so catch isn't going to work
+    alert('Location not found. Please try again.');
+
+  }
+}
